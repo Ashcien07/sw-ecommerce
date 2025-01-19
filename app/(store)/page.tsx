5837c878b9f99ad30205e629451cde5d272fb42f
Snehashish Patra <snehashishpatra8@gmail.com>
2025-01-19 18:21:28 +0530

    commit

---
+++ app/(store)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>
+        Hello WOrld!
+      </h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
